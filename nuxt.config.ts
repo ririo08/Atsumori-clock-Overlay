854cfc9aed01b09d55afc5cf4cadfa1a6ccd7916
ririo08 <ririomail@gmail.com>
2023-08-11 17:11:43 +0900

    chore: tailwindcss追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+
   css: ['assets/style.scss'],
+
   app: {
     head: {
       charset: 'utf-8',
