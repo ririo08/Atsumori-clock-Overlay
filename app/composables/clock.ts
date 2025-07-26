4d74c66161194db306b66d095229d224540ea849
ririo08 <7778943+ririo08@users.noreply.github.com>
2025-07-26 14:47:03 +0900

    feat(deps): Migrate nuxt4 (#116)
    * chore: upgrade Nuxt4
    
    * chore: use codemod migration

--- app/composables/clock.ts
+++ app/composables/clock.ts
@@ -6,12 +6,12 @@ export const useClock = () => {
     createClockString: (date: Date): Clock => {
       const dateArray = format(date, 'M,d,i,h,mm,aa').split(',')
       return {
-        month: dateArray[0],
-        day: dateArray[1],
-        week: Number(dateArray[2]) - 1,
-        hour: dateArray[3],
-        minute: dateArray[4],
-        ampm: dateArray[5],
+        month: dateArray[0]!,
+        day: dateArray[1]!,
+        week: Number(dateArray[2]!) - 1,
+        hour: dateArray[3]!,
+        minute: dateArray[4]!,
+        ampm: dateArray[5]!,
       }
     },
   }
